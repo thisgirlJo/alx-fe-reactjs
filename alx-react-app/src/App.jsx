0b0c69febb8cbc361237c7058415dbed12ea220a
Judith Ogbole <judithogbole71@gmail.com>
2025-08-11 19:45:05 +0100

    components in react

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,6 +3,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
+import Header from './Header'
+import MainContent from './MainContent'
+import Footer from './Footer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -29,6 +32,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Header />
+      <MainContent />
+      <Footer />
       <WelcomeMessage />
     </>
   )
