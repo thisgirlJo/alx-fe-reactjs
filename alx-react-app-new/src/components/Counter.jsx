9c0ff7b35ad657e96f9f5499852a55eed7d6e046
Judith Ogbole <judithogbole71@gmail.com>
2025-08-14 19:40:20 +0100

    Counter

---
+++ alx-react-app-new/src/components/Counter.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+
+function Counter() {
+    const [count, setCount] = useState(0);
+
+    return (
+        <div>
+            <p>Current Count: {count}</p>
+            <button onClick={() => setCount(count + 1)}>Increase</button>
+
+            <button onClick={() => setCount(count - 1)}>Decrease</button>
+
+            <button onClick={() => setCount(0)}>Reset</button>
+        </div>
+    );
+}
+export default Counter;
