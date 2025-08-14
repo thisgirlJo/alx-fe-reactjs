06125a37aaee50b5f00dd22895f9416e8186874d
Judith Ogbole <judithogbole71@gmail.com>
2025-08-14 19:30:58 +0100

    React Styling

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <div style={{ border: '1px solid gray', padding: '10px', margin: '10px'}}>
+      <footer>
+        <p>© 2023 City Lovers</p>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+function MainContent() {
+  return (
+    <div style={{ border: '1px solid gray', padding: '10px', margin: '10px'}} >
+      <main>
+        <p>I love to visit New York, Paris, and Tokyo.</p>
+      </main>
+    </div>
+  );
+}
+
+export default MainContent;
