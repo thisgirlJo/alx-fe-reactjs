5918534dce6ba0763608a45990de3c44802565d4
Judith Ogbole <judithogbole71@gmail.com>
2025-08-14 12:18:45 +0100

    User Profile

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -4,8 +4,9 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header';
-import MainContent from './MainContent'
+import MainContent from './components/MainContent'
 import Footer from './components/Footer'
+import UserProfile from './components/UserProfile'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -36,7 +37,7 @@ function App() {
       <WelcomeMessage />
       <MainContent />
       <Footer />
-
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
     </>
   )
 }
